349596debdf9408969ec931666a57c04b859c43a
Tamal Saha <tamal@appscode.com>
2024-09-29 17:12:00 -0700

    Generate image scripts
    Signed-off-by: Tamal Saha <tamal@appscode.com>

---
+++ catalog/export-images.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# Copyright AppsCode Inc. and Contributors
+#
+# Licensed under the AppsCode Community License 1.0.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://github.com/appscode/licenses/raw/1.0.0/AppsCode-Community-1.0.0.md
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -x
+
+mkdir -p images
+
+OS=$(uname -o)
+if [ "${OS}" = "GNU/Linix" ]; then
+    OS=Linux
+fi
+ARCH=$(uname -m)
+if [ "${ARCH}" = "aarch64" ]; then
+    ARCH=arm64
+fi
+curl -sL "https://github.com/google/go-containerregistry/releases/latest/download/go-containerregistry_${OS}_${ARCH}.tar.gz" >/tmp/go-containerregistry.tar.gz
+tar -zxvf /tmp/go-containerregistry.tar.gz -C /tmp/
+mv /tmp/crane images
+
+CMD="./images/crane"
+
+$CMD pull ghcr.io/kubevault/secrets-store-reader:v2024.9.30 images/kubevault-secrets-store-reader-v2024.9.30.tar
+$CMD pull ghcr.io/kubevault/vault-exporter:v0.1.1 images/kubevault-vault-exporter-v0.1.1.tar
+$CMD pull ghcr.io/kubevault/vault-operator:v0.19.0 images/kubevault-vault-operator-v0.19.0.tar
+$CMD pull kubevault/vault-unsealer:v0.19.0 images/kubevault-vault-unsealer-v0.19.0.tar
+$CMD pull vault:0.11.5 images/library-vault-0.11.5.tar
+$CMD pull vault:1.10.3 images/library-vault-1.10.3.tar
+$CMD pull vault:1.11.5 images/library-vault-1.11.5.tar
+$CMD pull vault:1.12.1 images/library-vault-1.12.1.tar
+$CMD pull vault:1.13.3 images/library-vault-1.13.3.tar
+$CMD pull vault:1.2.0 images/library-vault-1.2.0.tar
+$CMD pull vault:1.2.2 images/library-vault-1.2.2.tar
+$CMD pull vault:1.2.3 images/library-vault-1.2.3.tar
+$CMD pull vault:1.5.9 images/library-vault-1.5.9.tar
+$CMD pull vault:1.6.5 images/library-vault-1.6.5.tar
+$CMD pull vault:1.7.2 images/library-vault-1.7.2.tar
+$CMD pull vault:1.7.3 images/library-vault-1.7.3.tar
+$CMD pull vault:1.8.2 images/library-vault-1.8.2.tar
+$CMD pull vault:1.9.2 images/library-vault-1.9.2.tar
+
+tar -czvf images.tar.gz images
---
+++ catalog/import-images.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Copyright AppsCode Inc. and Contributors
+#
+# Licensed under the AppsCode Community License 1.0.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://github.com/appscode/licenses/raw/1.0.0/AppsCode-Community-1.0.0.md
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -x
+
+if [ -z "${IMAGE_REGISTRY}" ]; then
+    echo "IMAGE_REGISTRY is not set"
+    exit 1
+fi
+
+TARBALL=${1:-}
+tar -zxvf $TARBALL
+
+CMD="./crane"
+
+$CMD push images/kubevault-secrets-store-reader-v2024.9.30.tar $IMAGE_REGISTRY/kubevault/secrets-store-reader:v2024.9.30
+$CMD push images/kubevault-vault-exporter-v0.1.1.tar $IMAGE_REGISTRY/kubevault/vault-exporter:v0.1.1
+$CMD push images/kubevault-vault-operator-v0.19.0.tar $IMAGE_REGISTRY/kubevault/vault-operator:v0.19.0
+$CMD push images/kubevault-vault-unsealer-v0.19.0.tar $IMAGE_REGISTRY/kubevault/vault-unsealer:v0.19.0
+$CMD push images/library-vault-0.11.5.tar $IMAGE_REGISTRY/vault:0.11.5
+$CMD push images/library-vault-1.10.3.tar $IMAGE_REGISTRY/vault:1.10.3
+$CMD push images/library-vault-1.11.5.tar $IMAGE_REGISTRY/vault:1.11.5
+$CMD push images/library-vault-1.12.1.tar $IMAGE_REGISTRY/vault:1.12.1
+$CMD push images/library-vault-1.13.3.tar $IMAGE_REGISTRY/vault:1.13.3
+$CMD push images/library-vault-1.2.0.tar $IMAGE_REGISTRY/vault:1.2.0
+$CMD push images/library-vault-1.2.2.tar $IMAGE_REGISTRY/vault:1.2.2
+$CMD push images/library-vault-1.2.3.tar $IMAGE_REGISTRY/vault:1.2.3
+$CMD push images/library-vault-1.5.9.tar $IMAGE_REGISTRY/vault:1.5.9
+$CMD push images/library-vault-1.6.5.tar $IMAGE_REGISTRY/vault:1.6.5
+$CMD push images/library-vault-1.7.2.tar $IMAGE_REGISTRY/vault:1.7.2
+$CMD push images/library-vault-1.7.3.tar $IMAGE_REGISTRY/vault:1.7.3
+$CMD push images/library-vault-1.8.2.tar $IMAGE_REGISTRY/vault:1.8.2
+$CMD push images/library-vault-1.9.2.tar $IMAGE_REGISTRY/vault:1.9.2
