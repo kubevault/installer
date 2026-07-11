edac383feb39ae61b470fac1d28dae8351985cd8
Tamal Saha <tamal@appscode.com>
2026-07-11 16:32:50 +0600

    Modernize golangci-lint config
    - Enable bodyclose and prealloc linters
    - Move exclude-files/exclude-dirs to linters.exclusions.paths (golangci-lint
      v2 location) and fix over-escaped regex generated.*\\.go -> generated.*\.go
    - Switch formatter from gofmt to gofumpt and drop the interface{} -> any
      rewrite rule (gofumpt is a stricter superset)
    - Apply resulting gofumpt formatting
    
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- apis/installer/v1alpha1/types_test.go
+++ apis/installer/v1alpha1/types_test.go
@@ -26,7 +26,8 @@ import (
 )
 
 func TestDefaultValues(t *testing.T) {
-	checker := schemachecker.New(os.DirFS("../../.."),
+	checker := schemachecker.New(
+		os.DirFS("../../.."),
 		schemachecker.TestCase{Obj: v1alpha1.KubevaultCatalogSpec{}},
 		schemachecker.TestCase{Obj: v1alpha1.KubevaultOperatorSpec{}},
 		schemachecker.TestCase{Obj: v1alpha1.KubevaultWebhookServerSpec{}},
