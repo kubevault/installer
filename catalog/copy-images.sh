8d0e767723b35b2a64dbedd4e317810abfa88887
1gtm <1gtm@appscode.com>
2026-08-07 12:04:20 +0000

    Prepare for release v2026.8.7
    ProductLine: KubeVault
    
    Release: v2026.8.7
    
    Release-tracker: https://github.com/kubevault/CHANGELOG/pull/71
    Signed-off-by: 1gtm <1gtm@appscode.com>

--- catalog/copy-images.sh
+++ catalog/copy-images.sh
@@ -48,6 +48,6 @@ $CMD cp --allow-nondistributable-artifacts --insecure ghcr.io/appscode-images/va
 $CMD cp --allow-nondistributable-artifacts --insecure ghcr.io/appscode-images/vault:1.18.4 $IMAGE_REGISTRY/appscode-images/vault:1.18.4
 $CMD cp --allow-nondistributable-artifacts --insecure ghcr.io/appscode/kubectl-nonroot:1.34 $IMAGE_REGISTRY/appscode/kubectl-nonroot:1.34
 $CMD cp --allow-nondistributable-artifacts --insecure ghcr.io/kubevault/vault-exporter:v0.1.1 $IMAGE_REGISTRY/kubevault/vault-exporter:v0.1.1
-$CMD cp --allow-nondistributable-artifacts --insecure ghcr.io/kubevault/vault-operator:v0.25.0-rc.2 $IMAGE_REGISTRY/kubevault/vault-operator:v0.25.0-rc.2
-$CMD cp --allow-nondistributable-artifacts --insecure ghcr.io/kubevault/vault-unsealer:v0.25.0-rc.2 $IMAGE_REGISTRY/kubevault/vault-unsealer:v0.25.0-rc.2
+$CMD cp --allow-nondistributable-artifacts --insecure ghcr.io/kubevault/vault-operator:v0.25.0 $IMAGE_REGISTRY/kubevault/vault-operator:v0.25.0
+$CMD cp --allow-nondistributable-artifacts --insecure ghcr.io/kubevault/vault-unsealer:v0.25.0 $IMAGE_REGISTRY/kubevault/vault-unsealer:v0.25.0
 $CMD cp --allow-nondistributable-artifacts --insecure ghcr.io/sigilr/openbao:2.6.1-sigilr.1 $IMAGE_REGISTRY/sigilr/openbao:2.6.1-sigilr.1
