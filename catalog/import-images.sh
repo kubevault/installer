ef9f89db413fbbebd82bf767415e99bf1c80a3a5
Abdullah Al Masood <98589987+al-masood@users.noreply.github.com>
2026-08-07 11:09:37 +0600

    Add OpenBao Version 2.6.1 (#466)
    Signed-off-by: Abdullah Al Masood <masood@appscode.com>
    Signed-off-by: Tamal Saha <tamal@appscode.com>
    Co-authored-by: Tamal Saha <tamal@appscode.com>

--- catalog/import-images.sh
+++ catalog/import-images.sh
@@ -27,6 +27,7 @@ tar -zxvf $TARBALL
 CMD="./crane"
 
 $CMD push --allow-nondistributable-artifacts --insecure images/openbao-openbao-2.4.3.tar $IMAGE_REGISTRY/openbao/openbao:2.4.3
+$CMD push --allow-nondistributable-artifacts --insecure images/openbao-openbao-2.6.1.tar $IMAGE_REGISTRY/openbao/openbao:2.6.1
 $CMD push --allow-nondistributable-artifacts --insecure images/appscode-images-vault-1.10.3.tar $IMAGE_REGISTRY/appscode-images/vault:1.10.3
 $CMD push --allow-nondistributable-artifacts --insecure images/appscode-images-vault-1.11.5.tar $IMAGE_REGISTRY/appscode-images/vault:1.11.5
 $CMD push --allow-nondistributable-artifacts --insecure images/appscode-images-vault-1.12.1.tar $IMAGE_REGISTRY/appscode-images/vault:1.12.1
@@ -40,3 +41,4 @@ $CMD push --allow-nondistributable-artifacts --insecure images/appscode-kubectl-
 $CMD push --allow-nondistributable-artifacts --insecure images/kubevault-vault-exporter-v0.1.1.tar $IMAGE_REGISTRY/kubevault/vault-exporter:v0.1.1
 $CMD push --allow-nondistributable-artifacts --insecure images/kubevault-vault-operator-v0.25.0-rc.2.tar $IMAGE_REGISTRY/kubevault/vault-operator:v0.25.0-rc.2
 $CMD push --allow-nondistributable-artifacts --insecure images/kubevault-vault-unsealer-v0.25.0-rc.2.tar $IMAGE_REGISTRY/kubevault/vault-unsealer:v0.25.0-rc.2
+$CMD push --allow-nondistributable-artifacts --insecure images/sigilr-openbao-2.6.1-sigilr.1.tar $IMAGE_REGISTRY/sigilr/openbao:2.6.1-sigilr.1
